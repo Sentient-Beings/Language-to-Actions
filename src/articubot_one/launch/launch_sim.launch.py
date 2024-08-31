f6b8baf353efb9bc1ec2c6dbf0a9ba5c9f492d44
Muhammad540 <thisisdeahmed@gmail.com>
2024-08-31 18:12:35 +0300

    minor fixes for the diff drive vehicle

--- src/articubot_one/launch/launch_sim.launch.py
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -40,14 +40,12 @@ def generate_launch_description():
     #     )
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
-    world = os.path.join(get_package_share_directory(package_name), 'worlds', 'empty.world')
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file,
-                                      'world': world
-                                      }.items()
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
